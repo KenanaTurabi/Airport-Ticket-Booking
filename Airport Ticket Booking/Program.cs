d3e28ca19e980201740acd30a3fd04b04118a81e
KenanaTurabi <kenanaturabi@gmail.com>
2023-09-04 14:19:17 +0300

    adding constents to increase readability

--- Airport Ticket Booking/Program.cs
+++ Airport Ticket Booking/Program.cs
@@ -3,33 +3,48 @@ using Airport_Ticket_Booking.Models;
 using CsvHelper;
 using CsvHelper.Configuration;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace Airport_Ticket_Booking
 {
     internal class Program
     {
+        private const int ViewAllFlights=1;
+        private const int BookTicket=2;
+        private const int EditTicket=3;
+        private const int CancelBooking=4;
+        private const int ViewAllBookings=5;
+        private const int FilterBookingsByPrice=6;
+        private const int FilterBookingsByDepartureCountry = 7;
+        private const int FilterBookingsByDestinationCountry = 8;
+        private const int FilterBookingsByDepartureDate = 9;
+        private const int FilterBookingsByDepartureAirport = 10;
+        private const int FilterBookingsByArrivalAirport = 11;
+        private const int FilterBookingsByClass = 12;
+        private const int Exit= 13;
+
         public static void Menu()
         {
             Console.WriteLine($"----Airport Ticket Booking -----");
             Console.WriteLine();
             Console.WriteLine($"----Passenger Menu -----");
             Console.WriteLine();
-            Console.WriteLine($"1- see all flights");
-            Console.WriteLine($"2- book a ticket");
-            Console.WriteLine($"3- edit a ticket");
-            Console.WriteLine($"4- cancel a booking");
-            Console.WriteLine($"5- see all of your bookings");
+            Console.WriteLine($"{ViewAllFlights}- see all flights");
+            Console.WriteLine($"{BookTicket}- book a ticket");
+            Console.WriteLine($"{EditTicket}- edit a ticket");
+            Console.WriteLine($"{CancelBooking}- cancel a booking");
+            Console.WriteLine($"{ViewAllBookings}- see all of your bookings");
             Console.WriteLine();
             Console.WriteLine($"----Manager Menu -----");
             Console.WriteLine();
-            Console.WriteLine($"6- filter bookings by price");
-            Console.WriteLine($"7- filter bookings by DepartureCountry");
-            Console.WriteLine($"8- filter bookings by DestinationCountry");
-            Console.WriteLine($"9- filter bookings by DepartureDate");
-            Console.WriteLine($"10- filter bookings by DepartureAirport");
-            Console.WriteLine($"11- filter bookings by ArrivalAirport");
-            Console.WriteLine($"12- filter bookings by class");
-            Console.WriteLine($"13- Exit");
+            Console.WriteLine($"{FilterBookingsByPrice}- filter bookings by price");
+            Console.WriteLine($"{FilterBookingsByDepartureCountry}- filter bookings by DepartureCountry");
+            Console.WriteLine($"{FilterBookingsByDestinationCountry}- filter bookings by DestinationCountry");
+            Console.WriteLine($"{FilterBookingsByDepartureDate}- filter bookings by DepartureDate");
+            Console.WriteLine($"{FilterBookingsByDepartureAirport}- filter bookings by DepartureAirport");
+            Console.WriteLine($"{FilterBookingsByArrivalAirport}- filter bookings by ArrivalAirport");
+            Console.WriteLine($"{FilterBookingsByClass}- filter bookings by class");
+            Console.WriteLine($"{Exit}- Exit");
             Console.Write($"plz enter your choice:  ");
             
         }
@@ -47,7 +62,7 @@ namespace Airport_Ticket_Booking
                 using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
                 {
                     var flights = csv.GetRecords<Flight>();
-                    if (choice == 1)
+                    if (choice == ViewAllFlights)
                     {
                         foreach (var flight in flights)
                         {
@@ -60,7 +75,7 @@ namespace Airport_Ticket_Booking
 
                         }
                     }
-                    else if (choice == 2)
+                    else if (choice == BookTicket)
                     {
                         Console.Write("plz enter flight id to complete booking process:");
                         int EnteredFlightId =Int32.Parse(Console.ReadLine());
@@ -75,61 +90,62 @@ namespace Airport_Ticket_Booking
 
                         }
                     }
-                    else if (choice == 3)
+                    else if (choice == EditTicket)
                     {
                         Console.Write("plz enter booking id to edit the booking:");
                         int BookingIdToEdit = Int32.Parse(Console.ReadLine());
                         passenger.ModifyBooking(BookingIdToEdit,flights);
+                        
 
                         
                     }
-                    else if (choice == 4)
+                    else if (choice == CancelBooking)
                     {
                         Console.Write("plz enter booking id to cancle the booking:");
                         passenger.CancleBooking(Int32.Parse(Console.ReadLine()));
                     }
-                    else if (choice == 5)
+                    else if (choice == ViewAllBookings)
                     {
                         passenger.ViewBookings();
 
                     }
-                    else if (choice == 6)
+                    else if (choice == FilterBookingsByPrice)
                     {
                         manager.FilterBookings(SearchParameter.Price, AllBookingsList);
                         
                     }
-                    else if(choice == 7)
+                    else if(choice == FilterBookingsByDepartureCountry)
                     {
                         manager.FilterBookings(SearchParameter.DepartureCountry, AllBookingsList);
 
                     }
-                    else if (choice == 8)
+                    else if (choice == FilterBookingsByDestinationCountry)
                     {
                         manager.FilterBookings(SearchParameter.DestinationCountry, AllBookingsList);
 
                     }
-                    else if (choice == 9)
+                    else if (choice == FilterBookingsByDepartureDate)
                     {
                         manager.FilterBookings(SearchParameter.DepartureDate, AllBookingsList);
 
                     }
-                    else if (choice == 10)
+                    else if (choice == FilterBookingsByDepartureAirport)
                     {
                         manager.FilterBookings(SearchParameter.DepartureAirport, AllBookingsList);
 
                     }
-                    else if (choice == 11)
+                    else if (choice == FilterBookingsByArrivalAirport)
                     {
                         manager.FilterBookings(SearchParameter.ArrivalAirport, AllBookingsList);
 
                     }
-                    else if (choice == 12)
+                    else if (choice == FilterBookingsByClass)
                     {
                         manager.FilterBookings(SearchParameter.Class, AllBookingsList);
 
                     }
 
-                    else if(choice==13)
+                    else if(choice== Exit)
                     {
                         Console.WriteLine("EXIT");
                         break;
