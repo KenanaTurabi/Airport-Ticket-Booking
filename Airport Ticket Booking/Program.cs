1fbe30ffb88198ae4c2dacc9723e9c246f8e9efa
KenanaTurabi <kenanaturabi@gmail.com>
2023-09-20 11:04:14 +0300

    convert if-else statements into switch

--- Airport Ticket Booking/Program.cs
+++ Airport Ticket Booking/Program.cs
@@ -13,93 +13,74 @@ namespace Airport_Ticket_Booking
         static void Main(string[] args)
         {
             ReadDataFromCsv readDataFromCsv = new ReadDataFromCsv();
-            Passenger passenger=new Passenger() ;
+            Passenger passenger = new Passenger();
             DataManpulation dataManpulation = new DataManpulation(passenger);
             String filePath = "Flights.csv";
-            var flights= readDataFromCsv.ReadFromCsv(filePath);
-            AirportTicketMenu Menu =new AirportTicketMenu();
+            var flights = readDataFromCsv.ReadFromCsv(filePath);
+            AirportTicketMenu Menu = new AirportTicketMenu();
             Manager manager = new Manager();
             while (true)
             {
                 AirportTicketMenu.ViewMenu();
                 int choice = Int32.Parse(Console.ReadLine());
-                    foreach (var flight in flights)
-                    {
-                        flight.SetPriceAccordingToType();
-                    }
-                    if (choice == AirportTicketMenu.ViewAllFlights)
-                    {
-                    dataManpulation.ViewAllFlights(flights);
-                    }
-                    else if (choice == AirportTicketMenu.BookTicket)
-                    {
-                    Console.Write("plz enter flight id to complete booking process:");
-                    int FlightId = Int32.Parse(Console.ReadLine());
-                    dataManpulation.BookTicket(flights,FlightId);
-                    }
-                    else if (choice == AirportTicketMenu.EditTicket)
-                    {
-                    Console.Write("plz enter booking id to edit the booking:");
-                    int BookingIdToEdit = Int32.Parse(Console.ReadLine());
-                    dataManpulation.EditTicket(flights, BookingIdToEdit);
-                    }
-                    else if (choice == AirportTicketMenu.CancelBooking)
-                    {
-                    Console.Write("plz enter booking id to cancle the booking:");
-                    int BookingId = Int32.Parse(Console.ReadLine());
-                    dataManpulation.CancelBooking(BookingId);
-                    }
-                    else if (choice == AirportTicketMenu.ViewAllBookings)
-                    {
-                    dataManpulation.ViewAllBookings();
-                    }
-                    else if (choice == AirportTicketMenu.FilterBookingsByPrice)
-                    {
+                foreach (var flight in flights)
+                {
+                    flight.SetPriceAccordingToType();
+                }
+                switch (choice)
+                {
+                    case AirportTicketMenu.ViewAllFlights:
+                        dataManpulation.ViewAllFlights(flights);
+                        break;
+                    case AirportTicketMenu.BookTicket:
+                        Console.Write("plz enter flight id to complete booking process:");
+                        int FlightId = Int32.Parse(Console.ReadLine());
+                        dataManpulation.BookTicket(flights, FlightId);
+                        break;
+                    case AirportTicketMenu.EditTicket:
+                        Console.Write("plz enter booking id to edit the booking:");
+                        int BookingIdToEdit = Int32.Parse(Console.ReadLine());
+                        dataManpulation.EditTicket(flights, BookingIdToEdit);
+                        break;
+                    case AirportTicketMenu.CancelBooking:
+                        Console.Write("plz enter booking id to cancle the booking:");
+                        int BookingId = Int32.Parse(Console.ReadLine());
+                        dataManpulation.CancelBooking(BookingId);
+                        break;
+                    case AirportTicketMenu.ViewAllBookings:
+                        dataManpulation.ViewAllBookings();
+                        break;
+                    case AirportTicketMenu.FilterBookingsByPrice:
                         manager.FilterBookings(SearchParameter.Price, DataManpulation.AllBookingsList);
-                        
-                    }
-                    else if(choice == AirportTicketMenu.FilterBookingsByDepartureCountry)
-                    {
+                        break;
+                    case AirportTicketMenu.FilterBookingsByDepartureCountry:
                         manager.FilterBookings(SearchParameter.DepartureCountry, DataManpulation.AllBookingsList);
-
-                    }
-                    else if (choice == AirportTicketMenu.FilterBookingsByDestinationCountry)
-                    {
+                        break;
+                    case AirportTicketMenu.FilterBookingsByDestinationCountry:
                         manager.FilterBookings(SearchParameter.DestinationCountry, DataManpulation.AllBookingsList);
+                        break;
 
-                    }
-                    else if (choice == AirportTicketMenu.FilterBookingsByDepartureDate)
-                    {
+                    case AirportTicketMenu.FilterBookingsByDepartureDate:
                         manager.FilterBookings(SearchParameter.DepartureDate, DataManpulation.AllBookingsList);
-
-                    }
-                    else if (choice ==  AirportTicketMenu.FilterBookingsByDepartureAirport)
-                    {
+                        break;
+                    case AirportTicketMenu.FilterBookingsByDepartureAirport:
                         manager.FilterBookings(SearchParameter.DepartureAirport, DataManpulation.AllBookingsList);
-
-                    }
-                    else if (choice == AirportTicketMenu.FilterBookingsByArrivalAirport)
-                    {
+                        break;
+                    case AirportTicketMenu.FilterBookingsByArrivalAirport:
                         manager.FilterBookings(SearchParameter.ArrivalAirport, DataManpulation.AllBookingsList);
-
-                    }
-                    else if (choice == AirportTicketMenu.FilterBookingsByClass)
-                    {
+                        break;
+                    case AirportTicketMenu.FilterBookingsByClass:
                         manager.FilterBookings(SearchParameter.Class, DataManpulation.AllBookingsList);
-
-                    }
-
-                    else if(choice == AirportTicketMenu.Exit)
-                    {
+                        break;
+                    case AirportTicketMenu.Exit:
                         Console.WriteLine("EXIT");
                         break;
-                    }
-                    else
-                    {
+                    default:
                         Console.WriteLine("EXIT");
                         break;
-                    }                                            
-            }         
+
+                }
+            }
         }
     }
 }
